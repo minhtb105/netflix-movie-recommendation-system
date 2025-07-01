c8083f013b11530a05c4736a386698ed3fa95d38
root <root@DESKTOP-Q3VQC4Q.localdomain>
2025-07-01 15:24:29 +0700

    evaluation metrics and evaluation step

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,6 +3,7 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 class Evaluation(ABC):
+    @abstractmethod
     def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
         """
         Calculate the scores for the model
@@ -13,3 +14,109 @@ class Evaluation(ABC):
             None
         """
         pass
+    
+class MSE(Evaluation):
+    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+        """
+        Calculate Mean Squared Error
+        Args:
+            y_true: True labels
+            y_pred: predicted labels
+        Returns:
+            MSE score
+        """
+        try:
+            mse = np.mean((y_true - y_pred) ** 2)
+            logging.info(f'Mean Squared Error: {mse}')
+            
+            return mse
+        except Exception as e:
+            logging.error(f"Error while calculating MSE: {e}")
+            raise e 
+            
+class RMSE(Evaluation):
+    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+        """
+        Calculate Root Mean Squared Error
+        Args:
+            y_true: True labels
+            y_pred: predicted labels
+        Returns:
+            RMSE score
+        """
+        try:
+            mse = np.mean((y_true - y_pred) ** 2)
+            rmse = np.sqrt(mse)
+            logging.info(f'Root Mean Squared Error: {rmse}')
+            
+            return rmse
+        except Exception as e:
+            logging.error(f"Error while calculating RMSE: {e}")
+            raise e
+    
+class Procision(Evaluation):
+    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+        """
+        Calculate Precision
+        Args:
+            y_true: True labels
+            y_pred: predicted labels
+        Returns:
+            Precision score
+        """
+        try:
+            true_positive = np.sum((y_true == 1) & (y_pred == 1))
+            false_positive = np.sum((y_true == 0) & (y_pred == 1))
+            precision = true_positive / (true_positive + false_positive) if (true_positive + false_positive) > 0 else 0.0
+            
+            logging.info(f'Precision: {precision}')
+            
+            return precision
+        except Exception as e:
+            logging.error(f"Error while calculating Precision: {e}")
+            raise e
+        
+class Recall(Evaluation):
+    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+        """
+        Calculate Recall
+        Args:
+            y_true: True labels
+            y_pred: predicted labels
+        Returns:
+            Recall score
+        """
+        try:
+            true_positive = np.sum((y_true == 1) & (y_pred == 1))
+            false_negative = np.sum((y_true == 1) & (y_pred == 0))
+            recall = true_positive / (true_positive + false_negative) if (true_positive + false_negative) > 0 else 0.0
+            
+            logging.info(f'Recall: {recall}')
+            
+            return recall
+        except Exception as e:
+            logging.error(f"Error while calculating Recall: {e}")
+            raise e
+        
+class F1Score(Evaluation):
+    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+        """
+        Calculate F1 Score
+        Args:
+            y_true: True labels
+            y_pred: predicted labels
+        Returns:
+            F1 Score
+        """
+        try:
+            precision = Procision().calculate_scores(y_true, y_pred)
+            recall = Recall().calculate_scores(y_true, y_pred)
+            f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0.0
+            
+            logging.info(f'F1 Score: {f1_score}')
+            
+            return f1_score
+        except Exception as e:
+            logging.error(f"Error while calculating F1 Score: {e}")
+            raise e
+        
--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,12 +1,38 @@
 import logging
-import pandas as pd
-from zenml import step
+from src.model_dev import Model
+from src.evaluation import RMSE, MSE, Precision, Recall, F1Score
+import mlflow
 
-@step
-def evaluate_model(df: pd.DataFrame) -> None:
+
+def evaluate_model(model: Model, X_test, y_test) -> None:
     """
     Evaluates the model on the ingested data
     Args:
         df: the ingested data 
     """
-    pass
+    try:
+        y_pred = model.predict(X_test)
+        
+        rmse = RMSE()
+        rmse_score = rmse.calculate_scores(y_test, y_pred)
+        mlflow.log_metric("rmse", rmse_score)
+
+        mse = MSE()
+        mse_score = mse.calculate_scores(y_test, y_pred)
+        mlflow.log_metric("mse", mse_score)
+
+        precision = Precision()
+        precision_score = precision.calculate_scores(y_test, y_pred)
+        mlflow.log_metric("precision", precision_score)
+        
+        recall = Recall()
+        recall_score = recall.calculate_scores(y_test, y_pred)
+        mlflow.log_metric("recall", recall_score)
+        
+        f1_score = F1Score()
+        f1_score_value = f1_score.calculate_scores(y_test, y_pred)
+        mlflow.log_metric("f1_score", f1_score_value)
+    except Exception as e:
+        logging.error(f"Error while evaluating model: {e}")
+        raise e
+    
