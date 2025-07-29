0cb2dc0072914d1d073b393fd78200f428fbfef3
root <root@DESKTOP-Q3VQC4Q.localdomain>
2025-07-29 19:01:40 +0700

    chore(deps): add redis to setup.py requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="netflix_recommender",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="."), 
     include_package_data=True,
     install_requires=[
