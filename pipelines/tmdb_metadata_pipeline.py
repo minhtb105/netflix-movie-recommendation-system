e10869af75114e9555fcccf6877091cc877a9b33
root <root@DESKTOP-Q3VQC4Q.localdomain>
2025-08-02 14:58:15 +0700

    small change

--- pipelines/tmdb_metadata_pipeline.py
+++ pipelines/tmdb_metadata_pipeline.py
@@ -7,7 +7,7 @@ if str(project_root) not in sys.path:
 
 import json
 import asyncio
-from utils.downloader import async_batch_download_images, download_cast_images 
+from utils.downloader import async_batch_download_images, download_cast_images_batch 
 from services.movie_api import MovieService
 from services.tv_api import TVService
 
@@ -17,7 +17,7 @@ RAW_DIR.mkdir(parents=True, exist_ok=True)
 FEATURE_DIR.mkdir(parents=True, exist_ok=True)
 
 
-async def fetch_movie_ids(mv_service: MovieService, max_pages: int = 50):
+async def fetch_movie_ids(mv_service: MovieService, max_pages: int = 300):
     movie_ids = set()
     for func in [mv_service.fetch_popular, mv_service.fetch_top_rated, mv_service.fetch_upcoming]:
         for page in range(1, max_pages + 1):
@@ -53,7 +53,7 @@ async def fetch_movie_metadata(mv_service: MovieService, movie_ids: list[int],
     return all_meta
 
 
-async def fetch_tv_ids(tv_service: TVService, max_pages: int = 50):
+async def fetch_tv_ids(tv_service: TVService, max_pages: int = 300):
     tv_ids = set()
     for func in [tv_service.fetch_popular_tv, tv_service.fetch_top_rated_tv]:
         for page in range(1, max_pages + 1):
@@ -116,7 +116,7 @@ async def main():
                     })
                 
     await async_batch_download_images(movie_image_infos, save_dir="app/static/images/movie")
-    await download_cast_images(movie_cast_list)
+    await download_cast_images_batch(movie_cast_list)
 
     tv_ids = await fetch_tv_ids(tv_service)
     tv_meta = await fetch_tv_metadata(tv_service, tv_ids)
@@ -141,7 +141,7 @@ async def main():
                     })
                     
     await async_batch_download_images(tv_image_infos, save_dir="app/static/images/tv")
-    await download_cast_images(tv_cast_list)
+    await download_cast_images_batch(tv_cast_list)
 
     await mv_service.close()
     await tv_service.close()
