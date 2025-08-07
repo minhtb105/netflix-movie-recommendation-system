909c91f6841fe58cf420db45407942b9710448f1
root <root@DESKTOP-Q3VQC4Q.localdomain>
2025-08-07 11:27:34 +0700

    refactor(videos): change 'videos' array to single 'video' object and update access logic

--- pipelines/movies_pipeline_tmdb.py
+++ pipelines/movies_pipeline_tmdb.py
@@ -14,7 +14,7 @@ from src.data_strategy import BERTVectorizeStrategy
 import pandas as pd
 import numpy as np
 import yaml
-from datetime import datetime, UTC 
+from datetime import datetime, timezone
 
 
 def process_movies_pipeline(params: dict = None):
@@ -56,13 +56,13 @@ def process_movies_pipeline(params: dict = None):
     keep_cols = params['keep_cols']
     df = combine_features(df, vector_cols=df.columns, keep_cols=keep_cols)
     if "event_timestamp" not in df.columns:
-        df["event_timestamp"] = datetime.now(UTC)
-        review_df["event_timestamp"] = datetime.now(UTC)
+        df["event_timestamp"] = datetime.now(timezone.utc)
+        review_df["event_timestamp"] = datetime.now(timezone.utc)
         
     
     output_dir = Path(params['out_dir'])
     output_dir.mkdir(parents=True, exist_ok=True)
-    timestamp = datetime.now(UTC).strftime("%Y%m%dT%H%M%S")
+    timestamp = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%S")
     df.to_parquet(f"{output_dir}/movie_features_{timestamp}.parquet", index=False)
     review_df.to_parquet(f"{output_dir}/movie_reviews_{timestamp}.parquet", index=False)
 
--- pipelines/tv_pipeline_tmdb.py
+++ pipelines/tv_pipeline_tmdb.py
@@ -14,7 +14,7 @@ from src.data_strategy import BERTVectorizeStrategy
 import pandas as pd
 import numpy as np
 import yaml
-from datetime import datetime, UTC 
+from datetime import datetime, timezone
 
 
 def process_tv_pipeline(params: dict = None):
@@ -70,12 +70,12 @@ def process_tv_pipeline(params: dict = None):
     keep_cols = params['keep_cols']
     df = combine_features(df, vector_cols=df.columns, keep_cols=keep_cols)
     if "event_timestamp" not in df.columns:
-        df["event_timestamp"] = datetime.now(UTC)
-        review_df["event_timestamp"] = datetime.now(UTC)
+        df["event_timestamp"] = datetime.now(timezone.utc)
+        review_df["event_timestamp"] = datetime.now(timezone.utc)
    
     output_dir = Path(params['out_dir'])
     output_dir.mkdir(parents=True, exist_ok=True)
-    timestamp = datetime.now(UTC).strftime("%Y%m%dT%H%M%S")
+    timestamp = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%S")
     df.to_parquet(f"{output_dir}/tv_features_{timestamp}.parquet", index=False)
     review_df.to_parquet(f"{output_dir}/tv_reviews_{timestamp}.parquet", index=False)
 
