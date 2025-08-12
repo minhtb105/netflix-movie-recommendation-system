fbf4250017cf670cd8f9193c07e5651ec5c5789e
root <root@DESKTOP-Q3VQC4Q.localdomain>
2025-08-12 16:09:01 +0700

    remove unuse collect_images_for_collection function

--- services/tmdb_service/app/services/collection_api.py
+++ services/tmdb_service/app/services/collection_api.py
@@ -21,15 +21,3 @@ class CollectionService(TMDBBaseClient):
         params = {"include_image_language": "en,null"}
         
         return await self._get(endpoint, params)
-
-    def collect_images_for_collection(self, collection_id: int, save_dir: str):
-        data = self.get_collection_images(collection_id)
-        if not data:
-            logging.warning(f"No image data found for collection ID: {collection_id}")
-            return
-
-        save_dir = os.path.join(save_dir, str(collection_id))
-        os.makedirs(save_dir, exist_ok=True)
-
-        for img in data.get('backdrops', []) + data.get('posters', []):
-            download_image(img.get('file_path'), save_dir)
